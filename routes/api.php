911ae157328043b36245cc624f313db710350620
GMuktadir <gm.cse.duet@gmail.com>
2026-01-02 21:59:37 +0600

    healthCheck and fallback json response are working

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+//check api 
+Route::get('healthcheck',function(){
+    return response()->json([
+        'status' => 'success',
+        'message'=>'API is Running',
+        'data'=>[],
+        'error'=>[]
+    ]);
+});
+//when no route found | fallback function
+Route::fallback(function(){
+    return response()->json([
+        'status' => 'error',
+        'message'=>'API Endpoint not found (NO Route Found)',
+    ]);
+});
+
+Route::prefix('auth')->group(function () {
+    
+    });
