812f6925ef089d1ce75254e2176efa29fdc45b60
GMuktadir <gm.cse.duet@gmail.com>
2026-01-02 22:25:22 +0600

    failed validation overwrite in registrationRequest

---
+++ app/Http/Requests/Auth/RegistrationRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegistrationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //from false to true added by gm
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        //added by gm
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6|confirmed',
+        ];
+    }
+    //added by gm
+    protected function failedValidation(Validator $validator)
+    {
+         throw new HttpResponseException(response()->json([
+            'status' => 'Error',
+            'message'=>'Validation error',
+            'data'=>[],
+            'error'=>$validator->errors()
+            ], Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,5 +27,5 @@ Route::fallback(function(){
 });
 
 Route::prefix('auth')->group(function () {
-    
-    });
+    Route::post('register',[AuthController::class,'register']);
+});
